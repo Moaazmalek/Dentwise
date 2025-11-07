f48e0da9781e70726cc5e6ecf9ecba2933fc058e
moaaz malek <madhalfrwat@gmail.com>
2025-11-07 22:24:29 +0300

    Landing page completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,23 @@
 
-import { SignedIn, SignedOut, SignOutButton, SignUpButton } from '@clerk/nextjs'
-import React from 'react'
+import Header from '@/components/landing/Header'
+import Hero from '@/components/landing/Hero'
+import HowItWorks from '@/components/landing/HowItWorks'
+import WhatToAsk from '@/components/landing/WhatToAsk'
+import PricingSection from '@/components/landing/PricingSection'
+import CTA from '@/components/landing/CTA'
+import Footer from '@/components/landing/Footer'
+
 
 export default function Home() {
   return (
-    <div>
-    <h1> Home Page</h1>
-    <SignedOut>
-    <SignUpButton mode='modal'>
-      Sign Up
-      </SignUpButton>
-      </SignedOut>
-      <SignedIn>
-        <SignOutButton>
-          Logout
-        </SignOutButton>
-      </SignedIn>
+    <div className='min-h-screen bg-background '>
+      <Header/>
+      <Hero/>
+      <HowItWorks/>
+      <WhatToAsk/>
+      <PricingSection/>
+      <CTA/>
+      <Footer/>
     </div>
   )
 }
