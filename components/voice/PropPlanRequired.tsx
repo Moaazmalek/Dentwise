acf7ee17e4dc8af40ecdb17e749b25fc54e1f900
moaaz malek <madhalfrwat@gmail.com>
2025-12-05 16:27:31 +0300

    Remove Unnecessary navbar that cause error on build

--- components/voice/PropPlanRequired.tsx
+++ components/voice/PropPlanRequired.tsx
@@ -7,7 +7,6 @@ import Link from "next/link";
 function ProPlanRequired() {
   return (
     <div className="min-h-screen bg-background">
-      <Navbar />
 
       {/* Main Content */}
       <div className="max-w-7xl mx-auto px-6 py-8 pt-24">
